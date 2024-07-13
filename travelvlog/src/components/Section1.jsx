956118516984fa8cc5a10c90c6ce57bb18afdc06
Athira <athiramuralidharan219@gmail.com>
2024-07-13 23:36:38 +0530

    hh

--- travelvlog/src/components/Section1.jsx
+++ travelvlog/src/components/Section1.jsx
@@ -15,13 +15,13 @@ function Section1() {
                         Explore the world! <FaToolbox className='ml-1' />
                     </div>
                     <h1 className='text-lg font-semibold travel_text'>
-                        Travel <span className='span_texttop'>top destination <br /></span> of the world
+                        Travel <span className='span_texttop'>top destination <br /></span> <p className='ppp'>of the world</p>
                     </h1>
-                    <p className='p-section1'>
-                        We always make our customer happy by <br /> providing<br />
+                    <p className='p-section1 '>
+                        We always make our customer happy by providing
                         as many choices as possible
                     </p>
-                    <div className='flex space-x-4'>
+                    <div className='flex space-x-4 button'>
                         <button className='btn px-5 py-2 rounded-full flex items-center'>
                             Get Started
                         </button>
@@ -33,16 +33,16 @@ function Section1() {
                 <div className='section2'>
                     <div className='section1_2'>
                         <div className='ml-auto'>
-                            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 md:mb-40'>
                                 <div className='flex flex-col'>
-                                    <div className='p-4 mt-9 md:ms-5 rounded-md'>
+                                    <div className='p-4 mt-9 md:ms-5 rounded-md image'>
                                         <img src={img1} alt="Image 1"/>
                                     </div>
-                                    <div className='p-4 md:ms-5 rounded-md'>
+                                    <div className='p-4 md:ms-5 rounded-md image'>
                                         <img src={img3} alt="Image 3"/>
                                     </div>
                                 </div>
-                                <div className='p-4 rounded-md mt-32 md:mt-0'>
+                                <div className='p-4 rounded-md mt-32  image3'>
                                     <img src={img2} alt="Image 2"/>
                                 </div>
                             </div>
